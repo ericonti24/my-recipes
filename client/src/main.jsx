64c80e049a1e26faa44871ed2729e418e53e1369
Eric <ericontiveros123@gmail.com>
2026-08-14 11:47:30 -0700

    chakra ui import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { Provider } from './provider/Provider.jsx'
+import { Provider } from './provider/provider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
