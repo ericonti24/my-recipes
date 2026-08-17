d2737ab14d0fff7e00372b5ebc8a81e2275f958c
Eric <ericontiveros123@gmail.com>
2026-08-17 16:40:15 -0700

    input zoom fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './App.css'
 import { Provider } from './provider/provider.jsx'
 
 createRoot(document.getElementById('root')).render(
